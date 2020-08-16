40e9fe7c10b28479d19d3b5663c8910553c724cc
mustafapir <mustafapir29@gmail.com>
2020-08-17 00:29:26 +0300

    Fix a lot of bugs

--- analysis.R
+++ analysis.R
@@ -3,41 +3,14 @@ library(tidyr)
 library(splitstackshape)
 library(pbapply)
 library(stringr)
+library(svMisc)
 
-`%fin%` <- function(x, table) {
-  stopifnot(require(fastmatch))
-  fmatch(x, table, nomatch = 0L) > 0L
-}
-
-table<-fread("msa_table.txt")
-mouse<-fread("mouse_variations.txt") %>% separate("Refseq_ID", "Refseq_ID", sep = "\\.")
-
-mouse1<-str_split_fixed(mouse$`Amino Acid Change`, "->", 2)
-
-mouse$from<-mouse1[,1]
-mouse$to<-mouse1[,2]
-colnames(mouse)[26:27]<-c("from", "to")
-
-celegans<-fread("c_elegans_variations.csv")
-celegans1<-str_split_fixed(celegans$V7, " to ", 2)
-colnames(celegans1)<-c("from", "to")
-celegans<-cbind(celegans, celegans1)
-
-table33<-table[grep("musculus", table$fasta, ignore.case = TRUE),]
-table33<-table[grep("sapiens", table33$fasta, ignore.case = TRUE),]
-table33<-table[grep("elegans", table33$fasta, ignore.case = TRUE),]
-
-table2<-strsplit(as.character(table33$fasta), "\n>")
-
-table2<-pblapply(X = table2, FUN = function(t) gsub(pattern = "\n", replacement = "", x = t))
-
-table233<-pblapply(table2, function(x) length(x) == 3)
-table23<-table2[unlist(table233)]
-table23<-table23[1:length(table23)]
+source("convart_helper.R")
+source("data.R")
+source("analysis_function.R")
 
 
-lst1<-list()
-
+Sys.time()
 
 mouse_aap<-c()
 mouse_rfsq<-c()
@@ -95,9 +68,9 @@ for (i in 1:length(table23)){
     
     if (length(mouse_aapos) != 0){
       mouse_indx <- rep(i, length(mouse_aapos))
+      mouse_ind <- c(mouse_ind, mouse_indx)
     }
-    mouse_ind <- c(mouse_ind, mouse_indx)
-    
+
     
     mutagen_aapos<-c(mutagen$aa_position[which(mutagen$Refseq_ID == mouse_ag)])
     mutagen_sssqq<-which(mouse_anumber %fin% mutagen_aapos)
@@ -108,8 +81,9 @@ for (i in 1:length(table23)){
     
     if (length(mutagen_aapos) != 0){
       mutagen_indx <- rep(i, length(mutagen_aapos))
+      mutagen_ind <- c(mutagen_ind, mutagen_indx)
     }
-    mutagen_ind <- c(mutagen_ind, mutagen_indx)
+    
     
     
     # Human # 
@@ -120,7 +94,7 @@ for (i in 1:length(table23)){
      dbsnp_indx<-c()
      cosmic_indx<-c()
      gnomad_indx<-c()
-     human_a<-c(unlist(table23[[321943]][1]))
+     human_a<-c(unlist(table23[[i]][1]))
      human_a<-c(unlist(strsplit(human_a, "]")))
      human_ag<-c(unlist(strsplit(human_a, " \\["))[[1]][1])
      human_ag<-c(unlist(strsplit(human_ag, "\\."))[[1]][1])
@@ -136,28 +110,28 @@ for (i in 1:length(table23)){
      }
      
      clinvar_aapos<-c(clinvar$aapos[which(clinvar$Refseq_ID == human_ag)])
-     clinvar_sssqq<-which(human_anumber == clinvar_aapos)
+     clinvar_sssqq<-which(human_anumber %fin% clinvar_aapos)
      clinvar_sqsqsq<-c(clinvar_sqsqsq, clinvar_sssqq)
      clinvar_rrffssq<-clinvar$Refseq_ID[which(clinvar$Refseq_ID == human_ag)]
      clinvar_rfsq<-c(clinvar_rfsq, clinvar_rrffssq)
      clinvar_aap<-c(clinvar_aap, clinvar_aapos)
      
      dbsnp_aapos<-c(dbsnp$aapos[which(dbsnp$Refseq_ID == human_ag)])
-     dbsnp_sssqq<-which(human_anumber == dbsnp_aapos)
+     dbsnp_sssqq<-which(human_anumber %fin% dbsnp_aapos)
      dbsnp_sqsqsq<-c(dbsnp_sqsqsq, dbsnp_sssqq)
      dbsnp_rrffssq<-dbsnp$Refseq_ID[which(dbsnp$Refseq_ID == human_ag)]
      dbsnp_rfsq<-c(dbsnp_rfsq, dbsnp_rrffssq)
      dbsnp_aap<-c(dbsnp_aap, dbsnp_aapos)
      
      cosmic_aapos<-c(cosmic$aapos[which(cosmic$Refseq_ID == human_ag)])
-     cosmic_sssqq<-which(human_anumber == cosmic_aapos)
+     cosmic_sssqq<-which(human_anumber %fin% cosmic_aapos)
      cosmic_sqsqsq<-c(cosmic_sqsqsq, cosmic_sssqq)
      cosmic_rrffssq<-cosmic$Refseq_ID[which(cosmic$Refseq_ID == human_ag)]
      cosmic_rfsq<-c(cosmic_rfsq, cosmic_rrffssq)
      cosmic_aap<-c(cosmic_aap, cosmic_aapos)
      
      gnomad_aapos<-c(gnomad$aapos[which(gnomad$Refseq_ID == human_ag)])
-     gnomad_sssqq<-which(human_anumber == gnomad_aapos)
+     gnomad_sssqq<-which(human_anumber %fin% gnomad_aapos)
      gnomad_sqsqsq<-c(gnomad_sqsqsq, gnomad_sssqq)
      gnomad_rrffssq<-gnomad$Refseq_ID[which(gnomad$Refseq_ID == human_ag)]
      gnomad_rfsq<-c(gnomad_rfsq, gnomad_rrffssq)
@@ -165,23 +139,27 @@ for (i in 1:length(table23)){
     
      if (length(clinvar_aapos) != 0){
        clinvar_indx <- rep(i, length(clinvar_aapos))
+       clinvar_ind <- c(clinvar_ind, clinvar_indx)
      }
-     clinvar_ind <- c(clinvar_ind, clinvar_indx)
+     
      
      if (length(dbsnp_aapos) != 0){
        dbsnp_indx <- rep(i, length(dbsnp_aapos))
+       dbsnp_ind <- c(dbsnp_ind, dbsnp_indx)
      }
-     dbsnp_ind <- c(dbsnp_ind, dbsnp_indx)
+     
      
      if (length(cosmic_aapos) != 0){
        cosmic_indx <- rep(i, length(cosmic_aapos))
+       cosmic_ind <- c(cosmic_ind, cosmic_indx)
      }
-     cosmic_ind <- c(cosmic_ind, cosmic_indx)
+     
      
      if (length(gnomad_aapos) != 0){
        gnomad_indx <- rep(i, length(gnomad_aapos))
+       gnomad_ind <- c(gnomad_ind, gnomad_indx)
      }
-     gnomad_ind <- c(gnomad_ind, gnomad_indx)
+     
     
      
      # Celegans #
@@ -211,11 +189,14 @@ for (i in 1:length(table23)){
     
     if (length(celegans_aapos) != 0){
       celegans_indx <- rep(i, length(celegans_aapos))
+      celegans_ind <- c(celegans_ind, celegans_indx)
     }
-    celegans_ind <- c(celegans_ind, celegans_indx)
     
-    setTxtProgressBar(pb, i)
+    progress(i, 411668, init = (value == 1))
+    if (i == 411669) cat("Done!\n")
+    #setTxtProgressBar(pb, i)
 }
+Sys.time()
 
 # aapos = aa position of variations of one protein as given in the table
 # sqsqsq = index of aa positions in msa
@@ -223,35 +204,9 @@ for (i in 1:length(table23)){
 # aap = aa positions of variations
 # ind = index of protein in msa
 
-commonindMsa<-mouse_ind[which(mouse_ind %fin% celegans_ind)]
-commonindMouse<-which(mouse_ind %fin% commonindMsa)
-commonindCelegans<-which(celegans_ind %fin% commonindMsa)
 
-u_mouse_ind<-unique(mouse_ind)
-u_celegans_ind<-unique(celegans_ind)
-u_common_ind<-u_celegans_ind[which(u_celegans_ind %fin% u_mouse_ind)]
+# Analysis
 
-# Analysis - comparison
-
-txtt1<-c()
-msp4<-c()
-msp5<-c()
-msp6<-c()
-msp7<-c()
-ix<-c()
-pb = txtProgressBar(min = 0, max = length(u_common_ind), initial = 0)
-for (i in u_common_ind){
-  xmsp4<-mouse_aap[which(mouse_ind %fin% i) %fin% which(celegans_ind %fin% i)]
-  xmsp5<-mouse_rfsq[which(mouse_ind %fin% i) %fin% which(celegans_ind %fin% i)]
-  xmsp6<-celegans_aap[which(celegans_ind %fin% i) %fin% which(mouse_ind %fin% i)]
-  xmsp7<-celegans_rfsq[which(celegans_ind %fin% i) %fin% which(mouse_ind %fin% i)]
-  msp4<-c(msp, xmsp)
-  msp5<-c(msp1, xmsp1)
-  msp6<-c(msp2, xmsp2)
-  msp7<-c(msp3, xmsp3)
-  txtt1<-c(txtt1, i)
-  ix1<-c(ix1, i)
-  setTxtProgressBar(pb,length(txtt1))
-}
-
+mousecelegans<-analysis("mouse", "celegans")
+mutagencelegans<-analysis("mutagen", "celegans")
 
--- data.R
+++ data.R
@@ -1,19 +1,25 @@
 
 
+# !NOT RUN! #
+
 library(data.table)
 library(stringr)
 library(dplyr)
 library(seqinr)
 library(biomaRt)
 
+source("ens_np_blast.R")
+
+
+# msa
 
 table<-fread("msa_table.txt")
-
 table33<-table[grep("musculus", table$fasta, ignore.case = TRUE),]
 table33<-table[grep("sapiens", table33$fasta, ignore.case = TRUE),]
 table33<-table[grep("elegans", table33$fasta, ignore.case = TRUE),]
 
 table2<-strsplit(as.character(table33$fasta), "\n>")
+
 table2<-pblapply(X = table2, FUN = function(t) gsub(pattern = "\n", replacement = "", x = t))
 
 table233<-pblapply(table2, function(x) length(x) == 3)
@@ -21,6 +27,7 @@ table23<-table2[unlist(table233)]
 table23<-table23[1:length(table23)]
 
 
+# mouse
 
 mouse<-fread("mouse_variations.txt") %>% separate("Refseq_ID", "Refseq_ID", sep = "\\.")
 mouse1<-str_split_fixed(mouse$`Amino Acid Change`, "->", 2)
@@ -28,12 +35,16 @@ mouse$from<-mouse1[,1]
 mouse$to<-mouse1[,2]
 colnames(mouse)[26:27]<-c("from", "to")
 
+
+# celegans
+
 celegans<-fread("c_elegans_variations.csv")
 celegans1<-str_split_fixed(celegans$V7, " to ", 2)
 colnames(celegans1)<-c("from", "to")
 celegans<-cbind(celegans, celegans1)
 
 
+# clinvar
 
 clinvar<-fread("clinvar.csv")
 clinvar$V16<-gsub("\\.", "", clinvar$V16)
@@ -52,7 +63,8 @@ clinvar$to<-a(clinvar$to)
 colnames(clinvar)[17]<-"aapos"
 colnames(clinvar)[18]<-"Refseq_ID"
 
-# clinvar
+
+# dbsnp
 
 dbsnp<-fread("dbsnp.csv")
 dbsnp1<-str_split(dbsnp$V12, "\\.", simplify = TRUE)
@@ -68,41 +80,12 @@ dbsnp$to<-a(dbsnp$to)
 dbsnp$from<-a(dbsnp$from)
 dbsnp<-dplyr::select(dbsnp, V2, aapos, Refseq_ID, from, to)
 
-dbsnpw<-dbsnp
-dbsnpw$refseq<-all1$refseq[match(dbsnpw$Refseq_ID, all1$ens)]
-dbsnpwNoNA<-dbsnpw[!is.na(dbsnpw$refseq)]
-dbsnpwNA<-dbsnpw[is.na(dbsnpw$refseq)]
-enswo<-unique(dbsnpwNA$Refseq_ID)
-ensw<-unique(dbsnpwNoNA$Refseq_ID)
-totalens<-unique(dbsnp$Refseq_ID)
-
-glist21<-glist2[which(glist2$refseq_peptide != ""),]
-dbsnpw2<-merge(dbsnpw, glist21, by= "Refseq_ID", all = TRUE)
-dbsnpw$refseq2<-glist21$refseq_peptide[match(dbsnpw$Refseq_ID, glist21$Refseq_ID)]
-dbsnpw<- dbsnpw %>% mutate(refseq1= coalesce(refseq,refseq2))
-
-gene2ensembl<-fread("gene2ensembl", select = c(6,7))
-tempens<-str_split_fixed(gene2ensembl$Ensembl_protein_identifier, "\\.", 2)
-gene2ensembl$Ensembl_protein_identifier<-tempens[,1]
-gene2ensembl<-gene2ensembl[which(gene2ensembl$Ensembl_protein_identifier %in% totalens)]
+dbsnp$refseq<-all1$refseq[match(dbsnp$Refseq_ID, all1$ens)]
+dbsnp<-dbsnp[!is.na(dbsnp$refseq),]
+colnames(dbsnp)[c(3,6)]<-c("ens","Refseq_ID")
 
 
-lsts<-gconvert(c(totalens), organism = "hsapiens", target = "REFSEQ_PEPTIDE")
-
-ensembl <- useMart("ensembl", dataset = "hsapiens_gene_ensembl")
-mart <- useDataset("hsapiens_gene_ensembl", useMart("ensembl"))
-listAttributes(ensembl)
-glist3<-getBM(filters = "ensembl_transcript_id", 
-      attributes = c("ensembl_transcript_id","refseq_peptide"),
-      values = unique(cosmic$Refseq_ID) , mart = mart)
-colnames(glist2)[1]<-"Refseq_ID"
-dbsnp_f<-merge(dbsnp, glist2, by = "Refseq_ID", allow.cartesian = TRUE, all = TRUE)
-dbsnp_f<-dplyr::select(dbsnp_f, Refseq_ID, V2, aapos, V13, to, from, refseq_peptide)
-dbsnp_fu<-unique(dbsnp_f)
-dbsnp_funa<-dbsnp_fu[-which(dbsnp_fu$refseq_peptide == "")]
-
-# dbsnp_f
-
+# cosmic
 
 cosmic<-fread("CosmicMutantExport.csv", select = c(3, 20, 37)) # ENST, var, aapos
 
@@ -113,7 +96,17 @@ cosmic$from<-cosmic1[,1]
 cosmic$to<-apply(cosmic1, 1, function(x) paste0(x[2],x[3],x[4],x[5],x[6],x[7],x[8],x[9],x[10],x[11],x[12],x[13]))
 colnames(cosmic)[c(1,3)]<-c("Refseq_ID", "aapos")
 
-# cosmic 
+mart <- useDataset("hsapiens_gene_ensembl", useMart("ensembl"))
+glist<-getBM(filters = "ensembl_transcript_id", 
+              attributes = c("ensembl_transcript_id","refseq_peptide"),
+              values = unique(cosmic$Refseq_ID), mart = mart)
+cosmic$refseq<-glist$refseq_peptide[match(cosmic$Refseq_ID, glist$ensembl_transcript_id)]
+cosmic$refseq[which(cosmic$refseq == "")]<-NA
+cosmic<-cosmic[!is.na(cosmic$refseq),]
+colnames(cosmic)[c(1,6)]<-c("ens","Refseq_ID")
+
+
+# gnomad
 
 gnomad<-fread("gnomad.csv", select = c(4,5,7,9)) # ENST, var, RS_numb, aapos
 
@@ -127,13 +120,17 @@ gnomad1$from<-a(gnomad1$from)
 gnomad1$to<-a(gnomad1$to)
 colnames(gnomad1)[c(1,3,4)]<-c("Refseq_ID", "RS_ID", "aapos")
 
-glist1<-getBM(filters = "ensembl_transcript_id", 
-              attributes = c("ensembl_transcript_id","refseq_peptide"),
-              values = unique(gnomad1$Refseq_ID), mart = mart)
-colnames(glist_gnomad)[1]<-"Refseq_ID"
-gnomad_f<-merge(gnomad1, glist1_gnomad, by = "Refseq_ID", allow.cartesian = TRUE, all = TRUE)
+glist<-getBM(filters = "ensembl_transcript_id", 
+             attributes = c("ensembl_transcript_id","refseq_peptide"),
+             values = unique(gnomad1$Refseq_ID), mart = mart)
+
+gnomad1$refseq<-glist$refseq_peptide[match(gnomad1$Refseq_ID, glist$ensembl_transcript_id)]
+gnomad1$refseq[which(gnomad1$refseq == "")]<-NA
+gnomad<-gnomad1[!is.na(gnomad1$refseq),]
+colnames(gnomad)[c(1,7)]<-c("ens","Refseq_ID")
 
 
+# mutagen
 
 mutagen<-fread("mutagenetix_incidental_mutations_20190904.txt") %>%
   dplyr::select(aa_from, aa_to, aa_pos, fasta_header, sequence)
@@ -158,3 +155,7 @@ mutagenphe<-mutagenphe[!is.na(mutagenphe$Refseq_ID),]
 mutagenphe$type<-"phenotypic"
 
 mutagen<-rbind(mutagen, mutagenphe)
+
+tempmutagen<-str_split_fixed(mutagen$Refseq_ID , "\\.", 2)
+mutagen$Refseq_ID<-tempmutagen[,1]
+
